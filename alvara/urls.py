b285bca6fdba2f7ef4c0222e6b4215f819ee8cbb
Alineandrade <aline-0305@hotmail.com>
2014-11-18 11:17:10 -0200

    Configurando urls

--- alvara/urls.py
+++ alvara/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
+from cadastro.views import *
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'alvara.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('cadastro.views',
+url(r'^$', 'home', name='home'),
+url(r'^admin/', include(admin.site.urls)),
 )
+
